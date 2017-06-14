af5ac706226f9f42e136f470b14a552e01183e79
Brenda <bktran8@gmail.com>
2017-06-14 02:08:20 -0700

    add Tokenizer

---
+++ Tokenizer.hpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+class Tokenizer{
+
+std::vector<std::string>* GetTokens(const char *str, char d = ' ');
+};
