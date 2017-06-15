4f3157580a4c137ce397d36a47627ec8602231f7
Brenda <bktran8@gmail.com>
2017-06-15 00:28:28 -0700

    add Stack

---
+++ Stack.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Stack.hpp"
+using namespace std;
+
+template class Stack<int>;
+void Stack::Push(int d){
+    this ->data.push_back(int d);
+}
+
+template class Stack<int>;
+int Stack<int>::Pop(){
+    if(this->data.size() ==0){
+        return -1;
+    }
+    int ret = this ->data[this-.data.size()-1];
+    this->data.pop_back();
+    return ret;
+}
+template class Stack<int>;
+void Stack<int>::Print(){
+    cout<<"[ ";
+    for( int i=0; i<this-.data.size(); i++){
+        cout<<this->data[i] << " ";
+    }
+    cout << "]"<<endl;
+    
+}
