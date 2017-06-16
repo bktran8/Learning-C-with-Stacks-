858b501828ac99abff067ecd4b2ea603be10233e
Brenda <bktran8@gmail.com>
2017-06-16 05:31:35 -0700

    add Tokenizer

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -70,6 +70,7 @@ vector<string>* Tokenizer::GetTokens() {
             continue;
         } else if (num_Tokens==1){
             if(!strncmp(&token[0],"quit",4)) {
+                cout<<"k thx bye."<<endl;
                 exit(1);
             }
         }
