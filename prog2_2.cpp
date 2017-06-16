300abee33028220234873ded4421241f22c8ed12
Brenda <bktran8@gmail.com>
2017-06-16 10:33:08 -0700

    add prog2_2

---
+++ prog2_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include "Tokenizer.hpp"
+
+
+using namespace std;
+extern int input_Count;
+
+int main(int argc, char** argv) {
+    std::cout << "Assignment #2-1, Brenda Tran, bktran8@gmail.com" << endl;
+    if (argc >= 3 || argc == 0 || argc == 1) {
+        std::cerr << "ERROR! Program accepts 1 command line argument." << endl;
+        return 0;
+    } else if (strcmp(argv[1], "0") == 0) {
+        exit(1);
+    } else if ((atoi(argv[1])) == 0) {
+        std::cerr << "ERROR! Program expected integer argument." << endl;
+        return 0;
+    }
+    int N = (atoi(argv[1]));
+    while (input_Count <= N) {
+        vector<string> *temp = Tokenizer::GetTokens();
+        for (int i = 0; i < (*temp).size(); i++) {
+            cout << temp->at(i) << " ";
+        }
+        break;
+    }
+    return 0;
+}
