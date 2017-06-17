9fbe5572b17c4ca7857a68dd1534f79722fcb1b2
Brenda <bktran8@gmail.com>
2017-06-17 02:13:46 -0700

    add Tokenizer

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -72,8 +72,7 @@ vector<string>* Tokenizer::GetTokens() {
             continue;
         } else if (num_Tokens==1){
             if(!strncmp(&token[0],"quit",4)) {
-            (*temp).clear();
-                break;
+                exit(0);
             }
         }
         if((int_Count == 2)|| (str_Count ==2)){
