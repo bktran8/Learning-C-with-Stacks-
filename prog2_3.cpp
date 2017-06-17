0a212af46de45f99e2d49b96847db3d964e5ac10
Brenda <bktran8@gmail.com>
2017-06-17 10:51:58 -0700

    add prog2_3

--- prog2_3.cpp
+++ prog2_3.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <cstring>
 #include <cstdlib>
+#include <iostream>
 #include "Tokenizer.hpp"
 #include "Stack.hpp"
 
@@ -9,14 +10,14 @@ using namespace std;
 int input_Count;
 
 int main(int argc, char** argv) {
-    std::cout << "Assignment #2-2, Brenda Tran, bktran8@gmail.com" << endl;
+    cout << "Assignment #2-3, Brenda Tran, bktran8@gmail.com" << endl;
     if (argc >= 3 || argc == 0 || argc == 1) {
-        std::cerr << "ERROR! Program accepts 1 command line argument." << endl;
+        cerr << "ERROR! Program accepts 1 command line argument." << endl;
         return 0;
     } else if (strcmp(argv[1], "0") == 0) {
         exit(1);
     } else if ((atoi(argv[1])) == 0) {
-        std::cerr << "ERROR! Program expected integer argument." << endl;
+        cerr << "ERROR! Program expected integer argument." << endl;
         return 0;
     }
     int N = (atoi(argv[1]));
