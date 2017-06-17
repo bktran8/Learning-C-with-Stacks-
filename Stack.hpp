b2052a95b5d073e140ad8139de1a2028be167836
Brenda <bktran8@gmail.com>
2017-06-17 10:53:16 -0700

    add Stack

--- Stack.hpp
+++ Stack.hpp
@@ -1,37 +1,18 @@
-#include <iostream>
-
 using namespace std;
 
+template <class T>
 class Stack{
-  
-        int data;
-        void Push(int d);
-        int Pop();
-        void Print();
-        
-        
+public:
+    vector<T> data(int);
+
+
+    void push(T d);
+    int pop();
+    void print();
+
+
 };
 
-template class Stack<int>;
-void Stack::Push(int d){
-    this ->data.push_back(int d);
-}
 
-template class Stack<int>;
-int Stack<int>::Pop(){
-    if(this->data.size() ==0){
-        return -1;
-    }
-    int ret = this ->data[this-.data.size()-1];
-    this->data.pop_back();
-    return ret;
-}
-template class Stack<int>;
-void Stack<int>::Print(){
-    cout<<"[ ";
-    for( int i=0; i<this-.data.size(); i++){
-        cout<<this->data[i] << " ";
-    }
-    cout << "]"<<endl;
-    
-}
+
+
