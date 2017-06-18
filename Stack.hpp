6c787fc972f12a6f6108cc8a0d12d5c266fb3be3
Brenda <bktran8@gmail.com>
2017-06-17 19:49:19 -0700

    add Stack

--- Stack.hpp
+++ Stack.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 using namespace std;
 
 template <class T>
@@ -8,11 +9,30 @@ public:
 
 
     void Push(T d);
-    void Pop();
+    T Pop();
     void Print();
 
 
 };
+
+template <class T>
+void Stack<T>::Push(T d){
+    data.push_back(d);
+}
+
+template <class T>
+T Stack<T>::Pop(){
+    data.pop_back();
+}
+template <class t>
+void Stack<T>::Print(){
+    cout<<"[ ";
+    for( int i=0; i<data.size(); i++){
+        cout<< data[i] << " ";
+    }
+    cout << "]"<<endl;
+
+}
 
 
 
